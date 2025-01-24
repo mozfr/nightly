199520ec020c46aab5e4e357d280c4fadbb94261
Pascal Chevrel <pascal@mozilla.com>
2025-01-24 17:38:51 +0100

    Don't make setting a Github secret for CD a requirement locally

--- app/inc/init.php
+++ app/inc/init.php
@@ -9,12 +9,13 @@ date_default_timezone_set('Europe/Paris');
 // Load all constants for the application
 require_once __DIR__ . '/constants.php';
 
-// Load settings
-$settings_file = CONFIG . '/settings.inc.php';
-if (! file_exists($settings_file)) {
-    die('File app/config/settings.inc.php is missing. Please check your configuration.');
+// Load GitHub integration settings but not with the built-in dev server
+if (PHP_SAPI != 'cli-server') {
+    if (! file_exists(CONFIG . 'settings.inc.php')) {
+        die('File app/config/settings.inc.php is missing. Please check your configuration.');
+    }
+    require CONFIG . 'settings.inc.php';
 }
-require $settings_file;
 
 // Autoloading of classes (both /vendor and /classes)
 require_once INSTALL_ROOT . 'vendor/autoload.php';
